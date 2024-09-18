bd62c5c61b51247d1dce1bd79028c2b4688a54b3
mwangimuringi <mwangimuringi10@gmail.com>
2024-09-18 22:00:06 +0300

    ...

--- convex/auth.ts
+++ convex/auth.ts
@@ -8,8 +8,8 @@ import { DataModel } from "./_generated/dataModel";
 const CustomPassword = Password<DataModel>({
   profile(params) {
     return {
-      email: params.email as string,
-      name: params.name as string,
+     email: params.email as string,
+     name: params.name as string,
     };
   },
 });
