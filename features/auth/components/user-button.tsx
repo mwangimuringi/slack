d95e2f3ecf0d54dacedf81e2c95e8cde8ed8fa57
mwangimuringi <mwangimuringi10@gmail.com>
2024-09-18 23:55:02 +0300

    Returning workspaces of all the project

--- features/auth/components/user-button.tsx
+++ features/auth/components/user-button.tsx
@@ -8,7 +8,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Loader, LogOut } from "lucide-react";
-import useCurrentUser from "../hooks/use-current-user";
+import useCurrentUser from "../api/use-current-user";
 import { useAuthActions } from "@convex-dev/auth/react";
 
 const UserButton = () => {
