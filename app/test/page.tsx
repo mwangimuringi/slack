f99d641535ca6280a364a195e977d87da8d36c71
mwangimuringi <mwangimuringi10@gmail.com>
2024-09-18 10:31:56 +0300

    Fixed the errors

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -8,7 +8,6 @@ const Page = () => {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
     </main>
   );
 };
