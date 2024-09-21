4afd165265ec7e5641ced9e1b4dd491693776ec5
mwangimuringi <mwangimuringi10@gmail.com>
2024-09-21 17:29:46 +0300

    Added member functionality

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -1,6 +1,18 @@
+//in queries i will not throw errors while in mutations i will
 import { v } from "convex/values";
-import { mutation, query } from "./_generated/server";
 import { auth } from "./auth";
+import { mutation, query } from "./_generated/server";
+
+const generateCode = () => {
+  // const code = Math.random().toString(36).substring(2, 15);
+  // return code;
+  const code = Array.from(
+    { length: 6 },
+    () =>
+       "0123456789abcdefghijklmnopqrstuvwxyz" [Math.floor(Math.random() * 36)].toString()
+  ).join("");
+  return code;
+};
 
 //api endpoints  for workspaces
 export const create = mutation({
@@ -8,14 +20,14 @@ export const create = mutation({
     name: v.string(),
   },
   handler: async (ctx, args) => {
-    const userId =   await auth.getUserId(ctx);
+    const userId = await auth.getUserId(ctx);
 
     if (!userId) {
       throw new Error("Not authenticated");
     }
 
     //TODO: create a proper method later
-    const joinCode = "123456";
+    const joinCode = generateCode();
 
     const workspaceId = await ctx.db.insert("workspaces", {
       name: args.name,
@@ -23,7 +35,12 @@ export const create = mutation({
       joinCode,
     });
 
-    const workspace = await ctx.db.get(workspaceId);
+    // const workspace = await ctx.db.get(workspaceId);
+    await ctx.db.insert("members", {
+      userId,
+      workspaceId,
+      role: "admin",
+    });
 
     return workspaceId;
   },
@@ -32,19 +49,51 @@ export const create = mutation({
 export const get = query({
   args: {},
   handler: async (ctx) => {
-    return await ctx.db.query("workspaces").collect();
+    const userId = await auth.getUserId(ctx);
+    if (!userId) {
+      return [];
+    }
+
+    const members = await ctx.db
+      .query("members")
+      .withIndex("by_user_id", (q) => q.eq("userId", userId))
+      .collect();
+
+    const workspaceIds = members.map((member) => member.workspaceId);
+
+    const workspaces = [];
+    //generate a list of workspaces
+    for (const workspace of workspaceIds) {
+      const workspace = await ctx.db.get(workspaceId);
+
+      if (workspace) {
+        workspaces.push(workspace);
+      }
+    }
+    return workspaces;
   },
 });
 
 export const getById = query({
   args: {
-    id: v.id("workspaces")    
+    id: v.id("workspaces"),
   },
   handler: async (ctx, args) => {
     const userId = await auth.getUserId(ctx);
 
     if (!userId) {
       throw new Error("Not authenticated");
+    }
+
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.id).eq("userId", userId)
+      )
+      .unique();
+
+    if (!member) {
+      return null;
     }
 
     return await ctx.db.get(args.id);
