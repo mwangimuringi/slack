aa5a6ed85c0f88116a1d5b35c92bf86ad610358a
mwangimuringi <mwangimuringi10@gmail.com>
2024-09-24 23:57:48 +0300

    api-endpoint

--- convex/members.ts
+++ convex/members.ts
@@ -3,7 +3,28 @@ import { v } from "convex/values";
 import { query } from "./_generated/server";
 import { auth } from "./auth";
 
-export const current = query({
+export const get = query({
+    args: { workspaceId: v.id("workspaces") },
+    handler: async (ctx, args) => {
+        const userId = await auth.getUserId(ctx);
+
+        if (!userId) {
+            return [];
+        }
+        const members = await ctx.db
+            .query("members")
+            .withIndex("by_workspace_id_user_id", (q) =>
+                q.eq("workspaceId", args.workspaceId).eq("userId", userId)
+            )
+            .collect();
+
+        return members;
+    },
+})
+
+export 
+
+const current = query({
     args: { workspaceId: v.id("workspaces") },
     handler: async (ctx, args) => {
         const userId = await auth.getUserId(ctx);
