d2c15a4ecc0200ab798b279b3b94ebcab10498b2
mwangimuringi <mwangimuringi10@gmail.com>
2024-09-25 22:25:03 +0300

    api endpoints to get the channels

--- convex/channels.ts
+++ convex/channels.ts
@@ -1,6 +1,42 @@
 import { v } from "convex/values";
-import { query } from "./_generated/server";
+
 import { auth } from "./auth";
+import { mutation, query } from "./_generated/server";
+
+export const create  = mutation({
+  args: {
+    name: v.string(),
+    workspaceId: v.id("workspaces"),
+  },
+  handler: async (convexToJson, args) => {
+    const userId = await auth.getUserId(ctx);
+
+    if (!userId) {
+      throw new Error("Not authenticated");
+    }
+
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.workspaceId).eq("userId", userId)
+      )
+      .unique();
+
+      if (!member || member.role !== "admin") {
+        throw new Error("Not a member of the workspace");
+      }
+      //if validation is skipped in front end, then we need to validate on backend
+      const parseName = args.name
+       .replace(/\s+/g, "-")
+       .toLocaleLowerCase();
+
+       const channelId = await ctx.db.insert("channels", {
+        name: parseName,
+        workspaceId: args.workspaceId,
+       });
+       return channelId;
+  },
+});
 
 export const get = query({
   args: {
--- features/channels/components/create-channel-modal.tsx
+++ features/channels/components/create-channel-modal.tsx
@@ -1,48 +1,77 @@
-import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { ChangeEvent, FormEvent, useState } from "react";
 
-import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { toast } from "sonner";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
 
 import { useCreateChannelModal } from "@/features/channels/store/use-create-channel-modal";
+import { useCreateChannel } from "../api/use-create-channel";
 
 export const CreateChannelModal = () => {
-  const [open, setOpen] = useCreateChannelModal();
+  const router = useRouter();
+  const workspaceId = useWorkspaceId();
+
   const [name, setName] = useState("");
+
+  const { open, setOpen } = useCreateChannelModal();
+
+  const { mutateAsync, isPending } = useCreateChannel();
+
   const handleClose = () => {
     setName("");
     setOpen(false);
-  }
+  };
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value.replace(/\s+/g, "-").toLocaleLowerCase();
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.replace(/\s+/g, "-").toLowerCase();
     setName(value);
   };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    mutateAsync({
+      name,
+      workspaceId,
+    })
+      .then((channelId) => {
+        router.push(`/workspace/${workspaceId}/channel/${channelId}`);
+        handleClose();
+        toast.success("Channel created");
+      })
+      .catch((error) => {
+        console.error(error);
+        toast.error("Failed to create channel");
+      });
+  };
+
   return (
     <Dialog open={open} onOpenChange={handleClose}>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle> Add a channel </DialogTitle>
+          <DialogTitle>Add a channel</DialogTitle>
         </DialogHeader>
-        <form className="space-y-4">
+        <form className="space-y-4" onSubmit={handleSubmit}>
           <Input
             value={name}
-            disabled={false}
             onChange={handleChange}
-            required
+            disabled={isPending}
             autoFocus
+            placeholder="e.g. plan-budget"
+            required
             minLength={3}
             maxLength={80}
-            placeholder="e.g. plan-budget"
           />
           <div className="flex justify-end">
-            <Button disabled={false}>Create</Button>
+            <Button disabled={isPending}>Create</Button>
           </div>
         </form>
       </DialogContent>
