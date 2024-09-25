fd88c38128d46a9ea2faa95e633334a15dcb6be9
mwangimuringi <mwangimuringi10@gmail.com>
2024-09-25 21:32:02 +0300

    Added the functionality on channel modals

--- app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -12,6 +12,7 @@ import { useGetChannels } from "@/features/channels/api/use-get-channels";
 import { useGetMembers } from "@/features/members/api/use-get-member";
 import { useCurrentMember } from "@/features/members/api/use-current-member";
 import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
+import { useCreateChannelModal } from "@/features/channels/store/use-create-channel-modal";
 
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
 
@@ -19,11 +20,10 @@ import { UserItem } from "./user-item";
 import { SidebarItem } from "./sidebar-item";
 import { WorkspaceHeader } from "./workspace-header";
 import { WorkspaceSection } from "./workspace-section";
-import { useCreateChannelModal } from "@/features/channels/store/use-create-channel-modal";
 
 export const WorkspaceSidebar = () => {
   const workspaceId = useWorkspaceId();
-  const [] = useCreateChannelModal();
+  const [_open, setOpen] = useCreateChannelModal();
 
   const { data: member, isLoading: memberLoading } = useCurrentMember({
     workspaceId,
@@ -65,7 +65,7 @@ export const WorkspaceSidebar = () => {
         <SidebarItem label="Threads" icon={MessageSquareText} id="threads" />
         <SidebarItem label="Drafts & Sent" icon={SendHorizontal} id="drafts" />
       </div>
-      <WorkspaceSection label="Channels" hint="New channel" onNew={() => {}}>
+      <WorkspaceSection label="Channels" hint="New channel" onNew={member.role === "admin" ? () => setOpen(true) : undefined}>
         {channels?.map((item) => (
           <SidebarItem
             key={item._id}
--- features/channels/components/create-channel-modal.tsx
+++ features/channels/components/create-channel-modal.tsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
@@ -10,12 +14,37 @@ import { useCreateChannelModal } from "@/features/channels/store/use-create-chan
 
 export const CreateChannelModal = () => {
   const [open, setOpen] = useCreateChannelModal();
+  const [name, setName] = useState("");
+  const handleClose = () => {
+    setName("");
+    setOpen(false);
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.replace(/\s+/g, "-").toLocaleLowerCase();
+    setName(value);
+  };
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
+    <Dialog open={open} onOpenChange={handleClose}>
       <DialogContent>
         <DialogHeader>
           <DialogTitle> Add a channel </DialogTitle>
         </DialogHeader>
+        <form className="space-y-4">
+          <Input
+            value={name}
+            disabled={false}
+            onChange={handleChange}
+            required
+            autoFocus
+            minLength={3}
+            maxLength={80}
+            placeholder="e.g. plan-budget"
+          />
+          <div className="flex justify-end">
+            <Button disabled={false}>Create</Button>
+          </div>
+        </form>
       </DialogContent>
     </Dialog>
   );
