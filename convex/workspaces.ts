677d47c498d9cd137abc6455899da07a05461778
mwangimuringi <mwangimuringi10@gmail.com>
2024-09-26 11:48:39 +0300

    Created join-endpoint (mutation and hook)

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -12,6 +12,49 @@ const generateCode = () => {
   return code;
 };
 
+export const join = mutation({
+  args: {
+    joinCode: v.string(),
+    workspaceId: v.id("workspaces"),
+  },
+  handler: async (ctx, args) => {
+    const userId = await auth.getUserId(ctx);
+
+    if (!userId) {
+      throw new Error("Not authenticated");
+    }
+    //checking workspace
+    const workspace = await ctx.db.get(args.workspaceId);
+
+    if (!workspace) {
+      throw new Error("Workspace not found");
+    }
+
+    if (workspace.joinCode !== args.joinCode.toLowerCase()) {
+      throw new Error("Invalid join code");
+    }
+
+    const existingMember = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.workspaceId).eq("userId", userId)
+      )
+      .unique();
+
+    if (existingMember) {
+      throw new Error("User already joined workspace");
+    }
+
+    await ctx.db.insert("members", {
+      userId,
+      workspaceId: workspace._id,
+      role: "member",
+    });
+
+    return workspace._id;
+  },
+});
+
 export const newJoinCode = mutation({
   args: {
     workspaceId: v.id("workspaces"),
