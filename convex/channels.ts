09164e470881e556cab24bc3ac4778310e5e79ab
mwangimuringi <mwangimuringi10@gmail.com>
2024-09-27 11:42:38 +0300

    Added channel header query

--- convex/channels.ts
+++ convex/channels.ts
@@ -2,8 +2,9 @@ import { v } from "convex/values";
 
 import { auth } from "./auth";
 import { mutation, query } from "./_generated/server";
+import { userInfo } from "os";
 
-export const create  = mutation({
+export const create = mutation({
   args: {
     name: v.string(),
     workspaceId: v.id("workspaces"),
@@ -22,22 +23,50 @@ export const create  = mutation({
       )
       .unique();
 
-      if (!member || member.role !== "admin") {
-        throw new Error("Not a member of the workspace");
-      }
-      //if validation is skipped in front end, then we need to validate on backend
-      const parseName = args.name
-       .replace(/\s+/g, "-")
-       .toLocaleLowerCase();
+    if (!member || member.role !== "admin") {
+      throw new Error("Not a member of the workspace");
+    }
+    //if validation is skipped in front end, then we need to validate on backend
+    const parseName = args.name.replace(/\s+/g, "-").toLocaleLowerCase();
 
-       const channelId = await ctx.db.insert("channels", {
-        name: parseName,
-        workspaceId: args.workspaceId,
-       });
-       return channelId;
+    const channelId = await ctx.db.insert("channels", {
+      name: parseName,
+      workspaceId: args.workspaceId,
+    });
+    return channelId;
   },
 });
 
+export const getById = query({
+  args: {
+    id: v.id("channels"),
+  },
+  handler: async (ctx, args) => {
+    const userId = await auth.getUserId(ctx);
+
+    if (!userId) {
+      return null;
+    }
+    const channel = await ctx.db.get(args.id);
+
+    if (!channel) {
+      return null;
+    }
+
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", channel.workspaceId).eq("userId", userId)
+      )
+      .unique();
+
+    if (!member) {
+      return null;
+    }
+
+    return channel;
+  },
+});
 export const get = query({
   args: {
     workspaceId: v.id("workspaces"),
