28bc69b30af67aa967d47ce5bca1aa66b6135378
mwangimuringi <mwangimuringi10@gmail.com>
2024-09-30 22:28:10 +0300

    styling the editor

--- app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
+++ app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
@@ -1,5 +1,9 @@
+import Editor from "@/components/editor";
+
 export const ChatInput = () => {
     return(
-        <div>Chat Input</div>
+        <div className="px-5 w-full">Chat Input
+        <Editor />
+        </div>
     );
 };
---
+++ components/editor.tsx
@@ -0,0 +1,11 @@
+const Editor = () => {
+    return (
+        <div className="flex flex-col">
+             <div className="flex flex-col border border-slate-200  rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition bg-white">
+                <div className="">Editor</div>
+             </div>
+             </div>
+    );
+};
+
+export default Editor;
