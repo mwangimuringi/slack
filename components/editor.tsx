ae3c85dc3070314c13d09c694d00f47cee602580
mwangimuringi <mwangimuringi10@gmail.com>
2024-09-30 22:42:31 +0300

    Fixed errors

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,16 +1,42 @@
 import Quill from "quill";
+
 import "quill/dist/quill.snow.css";
+import { useRef } from "react";
 
 const Editor = () => {
-    const  containerRef = useRef<;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  const containerRef = useRef<HTMLDivElement>(null);
 
-    return (
-        <div className="flex flex-col">
-             <div className="flex flex-col border border-slate-200  rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition bg-white">
-                <div className="">Editor</div>
-             </div>
-             </div>
-    );
+  useEffect(() => {
+    if (!containerRef.current) return;
+
+    const quill = new Quill(containerRef.current, {
+      theme: "snow",
+      modules: {
+        toolbar: [
+          [{ header: [1, 2, false] }],
+          ["bold", "italic", "underline", "strike"],
+          [{ list: "ordered" }, { list: "bullet" }],
+          ["link", "image", "video"],
+          ["clean"],
+        ],
+      },
+    });
+
+    return () => {
+      quill.destroy();
+    };
+  }, []);
+
+  return (
+    <div className="flex flex-col">
+      <div className="flex flex-col border border-slate-200  rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition bg-white">
+        <div
+          ref={containerRef}
+          className="flex flex-col flex-1 overflow-hidden"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Editor;
