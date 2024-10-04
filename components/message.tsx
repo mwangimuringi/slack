745e483aec4e124d1ba632a59a329d704a488569
mwangimuringi <mwangimuringi10@gmail.com>
2024-10-04 19:13:41 +0300

    Added props to message components

---
+++ components/message.tsx
@@ -0,0 +1,50 @@
+import { Id, Doc } from "@/convex/_generated/dataModel";
+
+interface MessageProps {
+  id: Id<"messages">;
+  memberId: Id<"members">;
+  authorImage?: string;
+  authorName?: string;
+  isAuthor: boolean;
+  reactions: Array<
+    Omit<Doc<"reactions">, "memberId"> & {
+      count: number;
+      memberId: Id<"members">[];
+    }
+  >;
+  body: Doc<"messages">["body"];
+  image: string | null | undefined;
+  createdAt: Doc<"messages">["_creationTime"];
+  updatedAt: Doc<"messages">["updatedAt"];
+  isEditing: boolean;
+  isCompact?: boolean;
+  setEditingId: (id: Id<"messages"> | null) => void;
+  hideThreadButton?: boolean;
+  threadCount?: number;
+  threadImage?: string;
+  threadTimestamp?: number;
+}
+
+export const Message = ({
+    id,
+    isAuthor,
+    memberId,
+    authorImage,
+    authorName = "member",
+    reactions,
+    body,
+    image,
+    createdAt,
+    updatedAt,
+    isEditing,
+    isCompact,
+    setEditingId,
+    hideThreadButton,
+    threadCount,
+    threadImage,
+    threadTimestamp,
+}: MessageProps) => {
+    return (
+        <div className=""></div>
+    )
+};
