fb0ed3d6166d0a2dd22824779a7b121b8eac3f29
mwangimuringi <mwangimuringi10@gmail.com>
2024-10-04 16:40:48 +0300

    Deduped reactions on messages api

--- app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -2,6 +2,7 @@
 import { Loader, TriangleAlert } from "lucide-react";
 
 import { useGetChannel } from "@/features/channels/api/use-get-channel";
+import { useGetMessages } from "@/features/messages/api/use-get-messages";
 
 import { useChannelId } from "@/hooks/use-channel-id";
 
@@ -11,9 +12,12 @@ import { ChatInput } from "./chat-input";
 const ChannelPage = () => {
   const channelId = useChannelId();
 
+  const { results } = useGetMessages({ channelId });
   const { data: channel, isLoading: channelLoading } = useGetChannel({
     id: channelId,
   });
+
+  console.log({ results });
 
   if (channelLoading) {
     return (
@@ -37,10 +41,12 @@ const ChannelPage = () => {
   return (
     <div className="flex flex-col h-full">
       <Header title={channel.name} />
-      <div className="flex-1" />
+      <div className="flex-1" >
+        {JSON.stringify(results)}
+      </div>
       <ChatInput placeholder={`Message # ${channel.name}`} />
     </div>
-  )
+  );
 };
 
 export default ChannelPage;
