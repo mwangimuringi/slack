78a4dd21642b924f4a53944ef117eaf43590a5eb
mwangimuringi <mwangimuringi10@gmail.com>
2024-10-05 16:07:22 +0300

    Changed avatar's component css

--- components/message-list.tsx
+++ components/message-list.tsx
@@ -1,7 +1,9 @@
-import { format, isToday, isYesterday } from "date-fns";
+import { differenceInMinutes, format, isToday, isYesterday } from "date-fns";
 
 import { GetMessagesReturnType } from "@/features/messages/api/use-get-messages";
 import { Message } from "./message";
+
+const TIME_THRESHOLD = 5;
 
 interface MessageListProps {
   memberName?: string;
@@ -16,11 +18,11 @@ interface MessageListProps {
 }
 
 const formatDateLabel = (dateStr: string) => {
-    const date = new Date(dateStr);
-    if (isToday(date)) return "Today";
-    if (isYesterday(date)) return "Yesterday";
-    return format(date, "EEEE, MMMM d");
-}
+  const date = new Date(dateStr);
+  if (isToday(date)) return "Today";
+  if (isYesterday(date)) return "Yesterday";
+  return format(date, "EEEE, MMMM d");
+};
 
 export const MessageList = ({
   memberName,
@@ -33,56 +35,65 @@ export const MessageList = ({
   isLoadingMore,
   canLoadMore,
 }: MessageListProps) => {
-const groupedMessages = data?.reduce(
+  const groupedMessages = data?.reduce(
     (groups, message) => {
-        const date = new Date(message._creationTime);
-        const dateKey = format(date, "yyyy-MM-dd");
+      const date = new Date(message._creationTime);
+      const dateKey = format(date, "yyyy-MM-dd");
 
-        if (!groups[dateKey]) {
-            groups[dateKey] = [];
-        }
-        groups[dateKey].unshift(message);
-        return groups;
+      if (!groups[dateKey]) {
+        groups[dateKey] = [];
+      }
+      groups[dateKey].unshift(message);
+      return groups;
     },
-    {} as Record<string, typeof data>,
-);
+    {} as Record<string, typeof data>
+  );
 
   return (
     <div className="flex flex-1 flex-col-reverse pb-4 overflow-y-auto messages-scrollbar">
-     {Object.entries(groupedMessages || {}).map(([dateKey, messages]) => (
+      {Object.entries(groupedMessages || {}).map(([dateKey, messages]) => (
         <div key={dateKey}>
-            <div className="text-center my-2 relative">
-                <hr className="absolute top-1/2 left-0 right-0 border-t border-gray-300"/>
-                <span className="relative inline-block bg-white px-4 py-1 rounded-full text-xs border border-gray-300 shadow-sm">
-                    {formatDateLabel(dateKey)}
-                </span>
-            </div>
-            {messages.map((message, index) => {
-                return (
-                    <Message 
-                    key={message._id}
-                    id={message._id}
-                    memberId={message.memberId}
-                    authorImage={message.user.image}
-                    authorName={message.user.name}
-                    isAuthor={false}
-                    reactions={message.reactions}
-                    body={message.body}
-                    image={message.image}
-                    updatedAt={message.updatedAt}
-                    createdAt={message._creationTime}
-                    isEditing={false}
-                    setEditing={() => {}}
-                    isCompact={false}
-                    hideThreadButton={false}
-                    threadCount={message.threadCount}
-                    threadImage={message.threadImage}
-                    threadTimestamp={message.threadTimestamp}
-                    />
-                )
-            })}
+          <div className="text-center my-2 relative">
+            <hr className="absolute top-1/2 left-0 right-0 border-t border-gray-300" />
+            <span className="relative inline-block bg-white px-4 py-1 rounded-full text-xs border border-gray-300 shadow-sm">
+              {formatDateLabel(dateKey)}
+            </span>
+          </div>
+          {messages.map((message, index) => {
+            const prevMessage = messages[index - 1];
+            const isCompact = 
+            prevMessage &&
+            prevMessage.user?._id === message.user?._id &&
+            differenceInMinutes(
+                new Date(message._creationTime),
+                new Date(prevMessage._creationTime)
+            ) < TIME_THRESHOLD;
+
+            return (
+              <Message
+                key={message._id}
+                id={message._id}
+                memberId={message.memberId}
+                authorImage={message.user.image}
+                authorName={message.user.name}
+                isAuthor={false}
+                reactions={message.reactions}
+                body={message.body}
+                image={message.image}
+                updatedAt={message.updatedAt}
+                createdAt={message._creationTime}
+                isEditing={false}
+                setEditing={() => {}}
+                isCompact={isCompact}
+                hideThreadButton={false}
+                threadCount={message.threadCount}
+                threadImage={message.threadImage}
+                threadTimestamp={message.threadTimestamp}
+              />
+            );
+          })}
         </div>
-     ))}
+      ))}
     </div>
   );
 };
--- components/message.tsx
+++ components/message.tsx
@@ -64,31 +64,55 @@ export const Message = ({
               {format(new Date(createdAt), "hh:mm")}
             </button>
           </Hint>
-        </div>
+          <div className="flex flex-col w-full">
         <Renderer value={body} />
+        {updatedAt ? (
+          <span className="text-xs text-muted-foreground">
+            (edited)
+          </span>
+        ) : null}
+          </div>
+        </div>
       </div>
     );
   }
+  }
+
+  const avatarFallback = avatarName.charAt(0).toLocaleUpperCase();
+
   return (
     <div className="flex flex-col gap-2 p-1.5 px-5 hover:bg-gray-100/60 group relative">
       <div className="flex items-start gap-2">
         <button>
           <Avatar>
             <AvatarImage src={authorImage} />
-            <AvatarFallback>
-              {authorName?.charAt(0).toUpperCase()}
+            <AvatarFallback >
+              {avatarFallback}
             </AvatarFallback>
           </Avatar>
         </button>
       </div>
       <div className="flex flex-col w-full overflow-hidden">
         <div className="text-sm">
-          <button  onClick={() => {}} className="font-bold text-primary hover:underline">
+          <button
+            onClick={() => {}}
+            className="font-bold text-primary hover:underline"
+          >
             {authorName}
           </button>
+          <span>&nbsp;&nbsp;</span>
+          <Hint label={formatFullTime(new Date(createdAt))}>
+            <button className="text-xs text-muted-foreground hover:underline">
+              {format(new Date(createdAt), "h:mm a")}
+            </button>
+            <Hint/>
         </div>
-        <Renderer value={body} /> 
+        <Renderer value={body} />
+        {updatedAt? (
+          <span className="text-xs text-muted-foreground">{edited}</span>
+        ): null}
       </div>
     </div>
+  </div>
   );
 };
