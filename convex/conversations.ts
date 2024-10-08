250084e09ccb6adb5c75c6aa033dd8e01d11ee60
mwangimuringi <mwangimuringi10@gmail.com>
2024-10-08 23:56:07 +0300

    Fetched the members

--- convex/conversations.ts
+++ convex/conversations.ts
@@ -2,16 +2,23 @@ import { v } from "convex/values";
 import { mutation } from "./_generated/server";
 import { auth } from "./auth";
 
-export const createOrGet  = mutation({
-    args: {
-        memberId: v.id("members"),
-        workspaceId: v.id("workspaces"),
-    },
-    handler: async (ctx, args) => {
-        const userId =  await auth.getUserId(ctx);
+export const createOrGet = mutation({
+  args: {
+    memberId: v.id("members"),
+    workspaceId: v.id("workspaces"),
+  },
+  handler: async (ctx, args) => {
+    const userId = await auth.getUserId(ctx);
 
-        if (!userId) {
-            throw new Error("Unauthorized");
-        }
+    if (!userId) {
+      throw new Error("Unauthorized");
     }
+
+    const currentMember = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.workspaceId).eq("userId", userId)
+      )
+      .unique();
+  },
 });
