5964a273441a735cfd49fcc65f742c264eb8066c
mwangimuringi <mwangimuringi10@gmail.com>
2024-10-08 23:50:31 +0300

    Protecting the route

--- convex/conversations.ts
+++ convex/conversations.ts
@@ -1,8 +1,17 @@
+import { v } from "convex/values";
 import { mutation } from "./_generated/server";
+import { auth } from "./auth";
 
 export const createOrGet  = mutation({
-    args: {},
-    handler: async () => {
-        return {};
+    args: {
+        memberId: v.id("members"),
+        workspaceId: v.id("workspaces"),
+    },
+    handler: async (ctx, args) => {
+        const userId =  await auth.getUserId(ctx);
+
+        if (!userId) {
+            throw new Error("Unauthorized");
+        }
     }
 });
