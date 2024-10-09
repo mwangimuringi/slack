347f8e6f913042fdcb7b22137bd0f85aef876ba8
mwangimuringi <mwangimuringi10@gmail.com>
2024-10-09 23:51:50 +0300

    Unit testing

--- app/workspace/[workspaceId]/member/[memberId]/page.tsx
+++ app/workspace/[workspaceId]/member/[memberId]/page.tsx
@@ -13,11 +13,23 @@ const MemberPage = () => {
 
   const { data, mutate, isPending } = useCreateOrGetConversation();
 
+  console.log({ data });
+
   useEffect(() => {
-    mutate({
-      workspaceId,
-      memberId,
-    });
+    mutate(
+      {
+        workspaceId,
+        memberId,
+      },
+      {
+        onSuccess(data) {
+          console.log({ data });
+        },
+        onError(error) {
+          console.log({ error });
+        },
+      }
+    );
   }, [memberId, workspaceId, mutate]);
 
   if (isPending) {
@@ -32,12 +44,14 @@ const MemberPage = () => {
     return (
       <div className="h-full flex flex-col gap-y-2 items-center justify-center">
         <AlertTriangle className="size-6 text-muted-foreground" />
-            <span className="text-sm text-muted-foreground">Conversation not found</span>
+        <span className="text-sm text-muted-foreground">
+          Conversation not found
+        </span>
       </div>
     );
   }
 
-  return <div className="">{data}</div>;
+  return <div className="">{JSON.stringify({ data })}</div>;
 };
 
 export default MemberPage;
