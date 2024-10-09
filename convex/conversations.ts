d1c21f0482990b90b1bb7b7443bf5c7bdcea51fa
mwangimuringi <mwangimuringi10@gmail.com>
2024-10-09 23:21:46 +0300

    Created members conversation

--- convex/conversations.ts
+++ convex/conversations.ts
@@ -41,6 +41,21 @@ export const createOrGet = mutation({
             q.eq(q.field("memberTwoId"), currentMember._id)
           ),
         )
-      );
+      )
+      .unique();
+
+      const conversationId = await ctx.db.insert("conversations", {
+        workspaceId: args.workspaceId,
+        memberOneId: currentMember._id,
+        memberTwoId: otherMember._id,
+      });
+
+      const conversation = await ctx.db.get(conversationId);
+
+      if (!conversation) {
+        throw new Error("Conversation not found");
+      }
+
+    return conversation;
   },
 });
