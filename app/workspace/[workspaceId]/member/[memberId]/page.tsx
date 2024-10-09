699e3dcc02506fe6af0afd6390df874d305b020e
mwangimuringi <mwangimuringi10@gmail.com>
2024-10-09 23:45:30 +0300

    Stringfied the data

--- app/workspace/[workspaceId]/member/[memberId]/page.tsx
+++ app/workspace/[workspaceId]/member/[memberId]/page.tsx
@@ -14,29 +14,30 @@ const MemberPage = () => {
   const { data, mutate, isPending } = useCreateOrGetConversation();
 
   useEffect(() => {
-    mutate({ 
-        workspaceId, memberId 
-    })
+    mutate({
+      workspaceId,
+      memberId,
+    });
   }, [memberId, workspaceId, mutate]);
 
   if (isPending) {
     return (
-        <div className="h-full flex items-center justify-center">
-            <Loader className="size-6 animate-spin text-muted-foreground"/>
-        </div>
-    )
+      <div className="h-full flex items-center justify-center">
+        <Loader className="size-6 animate-spin text-muted-foreground" />
+      </div>
+    );
   }
 
-  
   if (!data) {
     return (
-        <div className="h-full flex items-center justify-center">
-            <AlertTriangle className="size-6 animate-spin text-muted-foreground"/>
-        </div>
-    )
+      <div className="h-full flex flex-col gap-y-2 items-center justify-center">
+        <AlertTriangle className="size-6 text-muted-foreground" />
+            <span className="text-sm text-muted-foreground">Conversation not found</span>
+      </div>
+    );
   }
 
-  return <div className="">{JSON.stringify({ memberId, workspaceId })}</div>;
+  return <div className="">{data}</div>;
 };
 
 export default MemberPage;
