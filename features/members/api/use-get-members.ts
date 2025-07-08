6d3643a4049a2a02cd6630c54036cde7be8a58d2
mwangimuringi <mwangimuringi10@gmail.com>
2025-07-08 11:44:44 +0300

    Created conversation component

--- features/members/api/use-get-members.ts
+++ features/members/api/use-get-members.ts
@@ -10,6 +10,7 @@ interface UseGetMembersProps {
 export const useGetMembers = ({ workspaceId }: UseGetMembersProps) => {
   const data = useQuery(api.members.get, { workspaceId });
   const isLoading = data === undefined;
+  console.log("data", data);
 
   return {
     data,
