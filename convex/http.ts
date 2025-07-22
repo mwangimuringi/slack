87419d843c78620c010ad6d14214d806b5e651fe
mwangimuringi <mwangimuringi10@gmail.com>
2025-07-22 15:01:40 +0300

    Created conversation component

--- convex/http.ts
+++ convex/http.ts
@@ -4,9 +4,5 @@ import { auth } from "./auth";
 const http = httpRouter();
 
 auth.addHttpRoutes(http);
-auth.addHttpRoutes(httpRouter());
-addEventListener("fetch", (event) => {
-  event.respondWith(http.handle(event.request));
-});
 
 export default http;
