3430d5d579cc44b163403247071eb194b1ecac6e
mwangimuringi <mwangimuringi10@gmail.com>
2025-07-24 15:16:20 +0300

    Returned member id

--- app/workspace/[workspaceId]/member/[memberId]/conversation.tsx
+++ app/workspace/[workspaceId]/member/[memberId]/conversation.tsx
@@ -3,15 +3,7 @@ import { Id } from "@/convex/_generated/dataModel";
 interface ConversationProps {
   id: Id<"conversations">;
 }
-export const Conversation = ({ id }: ConversationProps) => { 
-  const { data: conversation } = useQuery({
-    queryKey: ["conversation", id],
-    queryFn: async () => {
-      const { data } = await api.getById(id);
-      return data;
-    },
-  });
-  
+
 export const Conversation = ({ id }: ConversationProps) => {
   return (
   <div></div>
