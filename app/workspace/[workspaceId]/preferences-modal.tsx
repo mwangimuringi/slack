6f17fbcd3bf2aa4b56dcd0427794430f75ca7be2
mwangimuringi <mwangimuringi10@gmail.com>
2025-07-24 15:10:02 +0300

    ...

--- app/workspace/[workspaceId]/preferences-modal.tsx
+++ app/workspace/[workspaceId]/preferences-modal.tsx
@@ -42,7 +42,6 @@ export const PreferencesModal = ({
   const [editOpen, setEditOpen] = useState(false);
 
   const { mutate: updateWorkspace, isPending: isUpdatingWorkspace } =
-  console 
     useUpdateWorkspace();
   const { mutate: removeWorkspace, isPending: isRemovingWorkspace } =
     useRemoveWorkspace();
