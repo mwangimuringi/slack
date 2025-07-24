29aca9ea44742523be57a9ffb376f7bd392d0690
mwangimuringi <mwangimuringi10@gmail.com>
2025-07-24 15:16:44 +0300

    Styling the message component

--- app/workspace/[workspaceId]/member/[memberId]/conversation.tsx
+++ app/workspace/[workspaceId]/member/[memberId]/conversation.tsx
@@ -6,6 +6,11 @@ interface ConversationProps {
 
 export const Conversation = ({ id }: ConversationProps) => {
   return (
-  <div></div>
+  <div>
+    <div className="flex items-center gap-x-2 px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50">
+      <p className="text-sm font-semibold">Conversation name</p>
+      <p className="text-sm text-[#1264a3] hover:underline font-semibold">Edit</p>
+    </div>
+  </div>
 );
 };
