ac502bde2cc10bdb1d2e786ac6bc160e64806310
mwangimuringi <mwangimuringi10@gmail.com>
2025-07-24 15:17:19 +0300

    Stringfied the data

--- app/workspace/[workspaceId]/member/[memberId]/conversation.tsx
+++ app/workspace/[workspaceId]/member/[memberId]/conversation.tsx
@@ -10,6 +10,7 @@ export const Conversation = ({ id }: ConversationProps) => {
     <div className="flex items-center gap-x-2 px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50">
       <p className="text-sm font-semibold">Conversation name</p>
       <p className="text-sm text-[#1264a3] hover:underline font-semibold">Edit</p>
+      <TrashIcon className="w-4 h-4" />
     </div>
   </div>
 );
