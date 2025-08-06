9a4019e9a2f8e1a88a99faa32a332e2952f8a50d
mwangimuringi <mwangimuringi10@gmail.com>
2025-08-06 12:53:20 +0300

    Updated the styles

--- app/workspace/[workspaceId]/member/[memberId]/conversation.tsx
+++ app/workspace/[workspaceId]/member/[memberId]/conversation.tsx
@@ -10,8 +10,6 @@ export const Conversation = ({ id }: ConversationProps) => {
   <div>
     <div className="flex items-center gap-x-2 px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50">
       <p className="text-sm font-semibold">Conversation name</p>
-      <p className="text-sm text-[#1264a3] hover:underline font-semibold">View</p>
-      <TrashIcon className="w-4 h-4" />
       <p className="text-sm text-[#1264a3] hover:underline font-semibold">Edit</p>
       <TrashIcon className="w-4 h-4" /> 
     </div>
