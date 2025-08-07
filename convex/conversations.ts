0c0c874f0169220a767a378fa1123d0dda898e9b
mwangimuringi <mwangimuringi10@gmail.com>
2025-08-07 16:10:25 +0300

    api endpoint for uploading

--- convex/conversations.ts
+++ convex/conversations.ts
@@ -16,6 +16,8 @@ export const createOrGet = mutation({
 
     const currentMember = await ctx.db
       .query("members")
+      .withIndex("by_workspace_id_user_id", (q) => q.eq("workspaceId", args.workspaceId).eq("userId", userId))
+      .unique()
       .withIndex("by_workspace_id_user_id", (q) =>
         q.eq("workspaceId", args.workspaceId).eq("userId", userId)
       )
